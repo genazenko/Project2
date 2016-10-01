3c3fee914689ae0de8b0f85bd505ae8dd40f2466
qweasdzxc <qweasdzxc>
2016-10-01 17:53:50 +0300

    commit

--- AppInfo/src/main/java/by/bsu/zenko/model/AppInfo.java
+++ AppInfo/src/main/java/by/bsu/zenko/model/AppInfo.java
@@ -2,7 +2,6 @@ package by.bsu.zenko.model;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 /**
--- simple-web/src/main/java/by/bsu/zenko/web/MyServlet.java
+++ simple-web/src/main/java/by/bsu/zenko/web/MyServlet.java
@@ -3,17 +3,12 @@ package by.bsu.zenko.web;
 
 import by.bsu.zenko.model.AppInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
+
 public class MyServlet extends HttpServlet {
 
     @Override
